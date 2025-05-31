017655e8958e4646df81e1e15d781d383c71d8b7
VexarCoding <malzahnparker@>
2025-05-31 11:54:06 -0500

    Use aboveright split instead of default split command

--- lua/splice/sidebar.lua
+++ lua/splice/sidebar.lua
@@ -744,9 +744,9 @@ open_sidebar = function()
 
     -- Create a horizontal split at the bottom for the prompt area (roughly 20% of height)
     vim.api.nvim_win_call(sidebar_win, function()
-        vim.cmd("set nosplitbelow")
-        vim.cmd("split")
-        -- vim.cmd("resize -10")     -- Make it smaller
+        --vim.cmd("set nosplitbelow")
+        vim.cmd("aboveright split")
+        --vim.cmd("resize -10") -- Make it smaller
     end)
 
     -- Get the new window and set the prompt buffer
